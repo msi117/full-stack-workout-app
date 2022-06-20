8af5e39b0ff1bc2487019cc62adb47951431b83d
Mir Sameer <mir.sameer@example.com>
2022-06-20 18:36:46 +0530

    Initial Setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Server Running...');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colots = require('colors')
+const app = express()
+
+
+app.get('/',(req, res) => {
+    res.status(200).send('Welcome to homepage')
+})
+
+
+
+
+
+app.listen(process.env.PORT, process.env.HOST,() => {
+    console.log(`Server running on port ${process.env.PORT}`.blue.underline)
+})
